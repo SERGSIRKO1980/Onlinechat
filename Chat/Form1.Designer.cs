b337f8a6d27ed72ccf64fb80b76d2f08f5f55624
SERGSIRKO1980 <sergsirko1980@yahoo.com.ua>
2019-12-24 13:22:12 +0200

    Add project files.

---
+++ Chat/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroTextBox4 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 81);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(106, 23);
+            this.metroTextBox1.TabIndex = 0;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Location = new System.Drawing.Point(146, 81);
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.Size = new System.Drawing.Size(106, 23);
+            this.metroTextBox2.TabIndex = 1;
+            // 
+            // metroTextBox3
+            // 
+            this.metroTextBox3.Location = new System.Drawing.Point(258, 81);
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.Size = new System.Drawing.Size(119, 23);
+            this.metroTextBox3.TabIndex = 2;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(383, 81);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(25, 23);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "GO";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 107);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(68, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Local Port";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(146, 107);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(70, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Remote IP";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(273, 107);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(84, 19);
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "Remote Port";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(23, 373);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel5.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 145);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(385, 186);
+            this.listBox1.TabIndex = 9;
+            // 
+            // metroTextBox4
+            // 
+            this.metroTextBox4.Enabled = false;
+            this.metroTextBox4.Location = new System.Drawing.Point(23, 337);
+            this.metroTextBox4.Name = "metroTextBox4";
+            this.metroTextBox4.Size = new System.Drawing.Size(354, 23);
+            this.metroTextBox4.TabIndex = 10;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Enabled = false;
+            this.metroButton2.Location = new System.Drawing.Point(383, 337);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(25, 23);
+            this.metroButton2.TabIndex = 11;
+            this.metroButton2.Text = "S";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 412);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox4;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Chat/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Components;
+using MetroFramework.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Chat
+{
+    public partial class Form1 : MetroForm
+    {
+        
+        public static IPAddress remoteIP;
+        public static int localPort;
+        public static int remotePort;
+        public Thread tRec;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(tRec.IsAlive)
+            {
+                tRec.Abort();
+            }
+            Application.Exit();
+            System.Environment.Exit(1);
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            localPort = Int32.Parse(metroTextBox1.Text);
+            remoteIP = IPAddress.Parse(metroTextBox2.Text);
+            remotePort = Int32.Parse(metroTextBox3.Text);
+
+            metroLabel5.Text = "Status: Parsed";
+            Thread.Sleep(250);
+
+            if (localPort != 0 && remotePort != 0 && remoteIP != null)
+            {
+                metroTextBox4.Enabled = true;
+                metroButton2.Enabled = true;
+            }
+
+            tRec = new Thread(new ThreadStart(Receiver));
+            tRec.Start();
+            metroLabel5.Text = "Status: Whaiting for message...";
+        }
+        public void Send(string datagram)
+        {
+            // Создаем UdpClient
+            UdpClient sender = new UdpClient();
+
+            // Создаем endPoint по информации об удаленном хосте
+            IPEndPoint endPoint = new IPEndPoint(remoteIP, remotePort);
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            string lolcalIP = ipHost.AddressList[0].ToString();
+            datagram = lolcalIP + ": " + datagram;
+
+            try
+            {
+                // Преобразуем данные в массив байтов
+                byte[] bytes = Encoding.UTF8.GetBytes(datagram);
+
+                // Отправляем данные
+                sender.Send(bytes, bytes.Length, endPoint);
+                listBox1.Items.Add(datagram);
+            }
+            catch (Exception ex)
+            {
+                metroTextBox4.Text = ex.ToString();
+            }
+            finally
+            {
+                // Закрыть соединение
+                sender.Close();
+            }
+        }
+
+        public void Receiver()
+        {
+            // Создаем UdpClient для чтения входящих данных
+            UdpClient receivingUdpClient = new UdpClient(localPort);
+
+            IPEndPoint RemoteIpEndPoint = null;
+
+            try
+            {
+                listBox1.Items.Add(
+                   "-----------*******Общий чат*******-----------");
+
+                while (true)
+                {
+                    // Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(
+                       ref RemoteIpEndPoint);
+
+                    // Преобразуем и отображаем данные
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    listBox1.Items.Add(" --> " + returnData.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Send(metroTextBox4.Text);
+            metroLabel5.Text = "Status: Sent";
+            Thread.Sleep(300);
+            metroLabel5.Text = "Status: Whaiting for message...";
+            metroTextBox4.Clear();
+        }
+    }
+}
